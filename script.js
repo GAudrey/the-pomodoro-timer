975bcb70aef301bb1a2bea4e354ea78c4605d60e
GAudrey <gilmantaudrey.ag@gmail.com>
2020-12-16 15:01:38 +0100

    pomodoro timer ok with stop

--- script.js
+++ script.js
@@ -1,27 +1,61 @@
 let pomodoro = document.getElementById('pomodoro-timer');
 
-let sec = 60;
-let min25 = 24;
-let min5 = 4;
+let sec = 120;
+let min = Math.floor((sec/60));
+let displaySec = sec%60;
+let interval;
+let startBtnClicked = true;
 
-pomodoro.textContent = ("25 : 00");
+pomodoro.textContent = `${min} : ${displaySec}`;
+
+function displayTime(){
+    if (displaySec < 10){
+        pomodoro.textContent = `${min} : 0${displaySec}`;
+        console.log("sec < 10")
+        if (min < 10 && displaySec < 10){
+            pomodoro.textContent = `0${min} : 0${displaySec}`;
+            console.log("< 10 min and sec")
+        }
+    }
+    else if (min < 10){
+        pomodoro.textContent = `0${min} : ${displaySec}`;
+        console.log("min < 10")
+        if (min < 10 && displaySec < 10){
+            pomodoro.textContent = `0${min} : 0${displaySec}`;
+            console.log("< 10 min and sec")
+        }
+    }
+    else{
+        pomodoro.textContent = `${min} : ${displaySec}`;
+        console.log("tout baigne")
+    }
+}
+
+function startTimer() {
+    min = Math.floor((sec/60));
+    displaySec = sec%60;
+    sec--;
+    displayTime();
+    if (sec < 0){
+        stopTimer();
+    }
+}
+
+function stopTimer(){
+    clearInterval(interval)
+}
 
 startBtn.addEventListener('click', () =>{
-    setInterval(timer, 250);
-    function timer() {
-        sec--;
-        pomodoro.textContent = (min25 + " : " + sec);
-        if (sec == 60){
-            sec = "00";
-            sec--;
-        }
-        else if (sec == 0){
-            min25--;
-            sec = 60;
-        }
+    if (startBtnClicked == true){
+        startBtnClicked = false;
+        interval = setInterval(startTimer, 250);
+        startTimer()
     }
 })
 
 stopBtn.addEventListener('click', () =>{
-
+    if (startBtnClicked == false){
+        startBtnClicked = true;
+        stopTimer();
+    }
 })
